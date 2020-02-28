83eae5f0b30014cc0a5217b8423912ff164121b1
kmcalda <caldakevinmarvin@yahoo.com>
2020-02-28 17:38:09 +0800

    [UPDATE] made some improvement

--- models/laptops.py
+++ models/laptops.py
@@ -4,25 +4,26 @@ class laptops_devices(models.Model):
     _name = 'laptops.devices'
     _description = 'Laptop record'
     _rec_name = 'laptop_model'
+    _inherit = ['mail.thread', 'mail.activity.mixin']
 
     laptop_image = fields.Binary(string='Image')
-    laptop_user = fields.Many2one('res.users',string='User')
-    laptop_serial_number = fields.Char(string='Serial Number')
-    laptop_os = fields.Char(string='Operating System')
-    laptop_brand = fields.Char(string='Brand')
-    laptop_model = fields.Char(string='Model')
-    laptop_processor = fields.Char(string='Processor')
-    laptop_hd = fields.Char(string='HDD')
-    laptop_ram = fields.Char(string='RAM')
-    laptop_office = fields.Char(string='Office')
-    laptop_supplier = fields.Many2one('res.partner', string='Supplier')
-    laptop_market_value = fields.Float(string='Market Value', digits=(12,2))
-    laptop_purchase_date = fields.Date(string='Purchase Date')
-    laptop_warranty_expiration = fields.Date(string='Warranty Expire')
+    laptop_user = fields.Many2one('res.users',string='User', track_visibility='always', required=1)
+    laptop_serial_number = fields.Char(string='Serial Number', track_visibility='always', required=1)
+    laptop_os = fields.Char(string='Operating System', track_visibility='always')
+    laptop_brand = fields.Char(string='Brand', track_visibility='always')
+    laptop_model = fields.Char(string='Model name', track_visibility='always')
+    laptop_processor = fields.Char(string='Processor', track_visibility='always')
+    laptop_hd = fields.Char(string='HDD', track_visibility='always')
+    laptop_ram = fields.Char(string='RAM', track_visibility='always')
+    laptop_office = fields.Char(string='Office', track_visibility='always')
+    laptop_supplier = fields.Many2one('res.partner', string='Supplier', track_visibility='always')
+    laptop_market_value = fields.Float(string='Market Value', digits=(12,2), track_visibility='always')
+    laptop_purchase_date = fields.Date(string='Purchase Date', track_visibility='always')
+    laptop_warranty_expiration = fields.Date(string='Warranty Expire', track_visibility='always')
     laptop_condition = fields.Boolean(string='Unit in use', default=1)
     laptop_age = fields.Char(string='Age')
     laptop_note = fields.Text(string='Internal Note')
-    laptop_issued = fields.Date(string='Date issued')
+    laptop_issued = fields.Date(string='Date issued', track_visibility='always')
 
     _sql_constraints = [
         ('laptop_serial_number_unique',
