36b7cf7cfa43ff0b21f8dec6c0b7bd4a998628c1
kmcalda <caldakevinmarvin@yahoo.com>
2020-02-28 11:31:44 +0800

    [UPDATE] update some fields

--- models/desktops.py
+++ models/desktops.py
@@ -5,23 +5,24 @@ class desktops_devices(models.Model):
     _name = 'desktops.devices'
     _description = 'Desktop record'
     _rec_name = 'desktop_model'
+    _inherit = ['mail.thread', 'mail.activity.mixin']
 
-    desktop_user = fields.Many2one('res.users', string='User')
+    desktop_user = fields.Many2one('res.users', string='User', track_visibility='always', required=1)
     desktop_image = fields.Binary(string='Image')
-    desktop_serial_number = fields.Char(string='Serial Number')
-    desktop_model = fields.Char(string='Model')
-    desktop_os = fields.Char(string='Operating System')
-    desktop_processor = fields.Char(string='Processor')
-    desktop_hd = fields.Char(string='HDD')
-    desktop_ram = fields.Char(string='RAM')
-    desktop_office = fields.Char(string='Office')
-    desktop_supplier = fields.Char(string='Supplier')
-    desktop_market_value = fields.Float(string='Market Value', digit=(12,2))
-    desktop_purchase_date = fields.Date(string='Purchase Date')
-    desktop_warranty_expiration = fields.Date(string='Warranty Expiration')
-    desktop_condition = fields.Char(string='Condition')
+    desktop_serial_number = fields.Char(string='Serial Number', track_visibility='always', required=1)
+    desktop_model = fields.Char(string='Model name', track_visibility='always')
+    desktop_os = fields.Char(string='Operating System', track_visibility='always')
+    desktop_processor = fields.Char(string='Processor', track_visibility='always')
+    desktop_hd = fields.Char(string='HDD', track_visibility='always')
+    desktop_ram = fields.Char(string='RAM', track_visibility='always')
+    desktop_office = fields.Char(string='Office', track_visibility='always')
+    desktop_supplier = fields.Many2one('res.partner', string='Supplier', track_visibility='always')
+    desktop_market_value = fields.Float(string='Market Value', digit=(12,2), track_visibility='always')
+    desktop_purchase_date = fields.Date(string='Purchase Date', track_visibility='always')
+    desktop_warranty_expiration = fields.Date(string='Warranty Expiration', track_visibility='always')
+    desktop_condition = fields.Boolean(string='Unit in use')
     desktop_age = fields.Char(string='Age')
-    desktop_issued = fields.Date(string='Date issued')
+    desktop_issued = fields.Date(string='Date issued', track_visibility='always')
 
     _sql_constraints = [
         ('desktop_serial_number_unique',
